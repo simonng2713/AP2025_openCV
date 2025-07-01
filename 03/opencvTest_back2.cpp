61b62dda2793d59f95cba762a94bad0210fce4c8
Simon Ng <simon.kah.seng@misc.mech.eng.osaka-u.ac.jp>
2025-07-01 11:22:50 +0900

    20250701
    Added trackbar for threshold in bgfs

--- 03/opencvTest_back2.cpp
+++ 03/opencvTest_back2.cpp
@@ -6,7 +6,15 @@ using namespace cv;
 
 VideoCapture cap;
 
+int varThreshold = 20;
+cv::Ptr<cv::BackgroundSubtractorMOG2> bgfs;
+
 void on_tracker(int p, void *) { cap.set(CAP_PROP_POS_FRAMES, p); }
+
+void on_threshold(int p, void *) {
+  varThreshold = p;
+  if (bgfs){bgfs->setVarThreshold(varThreshold);};
+}
 
 int main() {
   cap.open("vtest.avi");
@@ -16,21 +24,23 @@ int main() {
   }
 
   Mat src, dst, back, segm;
-  namedWindow("movie", WINDOW_AUTOSIZE);
+  namedWindow("movie", WINDOW_GUI_EXPANDED);
   setWindowProperty("movie", WND_PROP_TOPMOST, 1);
   createTrackbar("pos", "movie", nullptr, (int)cap.get(CAP_PROP_FRAME_COUNT),
                  on_tracker);
   setTrackbarPos("pos", "movie", 0);
 
+
   bool playing = true;
   bool loopflag = true;
 
   int history = 100;
-  float varThreshold = 20;
   bool bShadowDetection = false;
-  cv::Ptr<cv::BackgroundSubtractor> bgfs =
-      createBackgroundSubtractorMOG2(history, varThreshold, bShadowDetection);
 
+  bgfs = createBackgroundSubtractorMOG2(history, varThreshold, bShadowDetection);
+      
+  createTrackbar("threshold", "movie", nullptr, 500, on_threshold);
+  
   while (loopflag) {
     if (playing && cap.read(src)) {
       bgfs->apply(src, back);
